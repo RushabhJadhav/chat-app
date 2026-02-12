7d2862f0b3eb230b5dbc088fe0f966946dbdd425
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-02-12 17:48:13 +0530

    User route controller & model setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
 
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
 const app = express();
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/api/auth", authRoutes);
+
 app.listen(PORT, () => {
-    console.log(`Application started on PORT: ${process.env.PORT}`)
-})
+    console.log(`Server is running on PORT: ${process.env.PORT}`)
+    connectDB();
+});
