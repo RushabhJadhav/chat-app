5faab57f660319ce72b6bbcacdc1c3764eb3e683
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-02-17 22:03:19 +0530

    router setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+
 const App = () => {
 
   return (
     <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/settings" element={<SettingsPage />}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
+      </Routes>
     </div>
   )
 };
