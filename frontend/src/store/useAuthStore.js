96a41dbe487e277e82ae764026b9ed26183893ea
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-03-01 13:02:07 +0530

    nav page and login page udpated

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -33,6 +33,20 @@ export const useAuthStore = create((set) => ({
       set({isSigingUp: false})
     }
   },
+  login: async (data) => {
+    set({ isLoggingIn: true });
+    try {
+      const res = await axiosInstance.post("/auth/login", data);
+      set({ authUser: res.data });
+      toast.success("Logged in successfully");
+
+      get().connectSocket();
+    } catch (error) {
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isLoggingIn: false });
+    }
+  },
   logout: async () => {
     try {
       await axiosInstance.post("/auth/logout");
