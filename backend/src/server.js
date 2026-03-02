c36bca047eba64b843f657db077cba196ed5c37f
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-03-02 16:21:09 +0530

    file upload fix and themes setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 app.use(cookieParser());
 
