d1475ffbe07ed1d5d63a9009e6b2fb02f408a07e
Rushabh Jadhav <rushabhjadhav666@gmail.com>
2026-03-05 01:26:20 +0530

    added user chat store

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
+
+export const useChatStore = create((set) => ({
+  messages: [],
+  users: [],
+  selectedUser: null,
+  isUsersLoading: false,
+  isMessagesLoading: false,
+  getUsers: async () => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get("/messages/users");
+      set({ users: res.data });
+    } catch (error) {
+      console.log(error);
+      toast.error(error.resonse.data.message);
+    } finally {
+      set({ isUsersLoading: true });
+    }
+  },
+  getMessages: async (userId) => {
+    set({ isUsersLoading: true });
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
+    } catch (error) {
+      console.log(error);
+      toast.error(error.resonse.data.message);
+    } finally {
+      set({ isUsersLoading: false });
+    }
+  }
+}));
